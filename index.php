e5cd3cb72b9b036097bf16e403e2b1bd8343c848
Arthur Jenck <arthurjenckdev@gmail.com>
2025-02-18 12:53:45 +0100

    feat: added bad code to debug

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+$users = [
+    [
+        'full_name' => 'Mickaël Andrieu',
+        'email' => 'mickael.andrieu@exemple.com'
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Mathieu Nebra',
+        'email' => 'mathieu.nebra@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+    ],
+];
+
 $recipes = [
     [
         'title' => 'Cassoulet',
@@ -18,77 +36,59 @@ $recipes = [
         'author' => 'mathieu.nebra@exemple.com',
         'is_enabled' => true,
     ],
+    [
+        'title' => 'Salade Romaine',
+        'recipe' => 'Etape 1 : prenez une belle salade',
+        'author' => 'lorene.castor@exemple.com',
+        'is_enablad' => true,
+    ],
 ];
 
-$users = [
- [
-   'full_name' => 'Mickaël Andrieu',
-   'email' => 'mickael.andrieu@exemple.com',
-   'age' => 34,
-  ],
-  [
-   'full_name' => 'Mathieu Nebra',
-   'email' => 'mathieu.nebra@exemple.com',
-   'age' => 34,
-  ],
-  [
-   'full_name' => 'Laurène Castor',
-   'email' => 'laurene.castor@exemple.com',
-   'age' => 28,
-  ],
- ];
-
-
-function isValidRecipe(array $recipe) : bool
-{
-    if (array_key_exists('is_enabled', $recipe)) {
-        $isEnabled = $recipe['is_enabled'];
-    } else {
-        $isEnabled = false;
-    }
-
-    return $isEnabled;
-}
-
-function getRecipes(array $recipes) : array
-{
-    $validRecipes = [];
-
-    foreach($recipes as $recipe) {
-        if (isValidRecipe($recipe)) {
-            $validRecipes[] = $recipe;
-        }
-    }
-
-    return $validRecipes;
-}
- 
 function displayAuthor(string $authorEmail, array $users): string
-{
-    foreach ($users as $user) {
-        if ($authorEmail === $user['email']) {
-            return $user['full_name'] . '(' . $user['age'] . ' ans)';
+    {
+        //var_dump($authorEmail); die();
+        foreach($users as $user) {
+            if ($authorEmail === $user['email']) {
+                return $user['full_name'] . '(' . $user['age'] . ' ans)';
+            }
         }
     }
+function isValidRecipe(array $recipe): bool
+    {
+        return $recipe['is_enabled'];
+    }
+function getRecipes(array $recipes) : array
+    {
+        $valid_recipes = [];
+        foreach($recipes as $recipe) {
+            if (isValidRecipe($recipe)) {
+                $valid_recipes[] = $recipe;
+            }
+        }
+    return $valid_recipes;
 }
-
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Affichage des recettes</title>
+    <title>Recettes de cuisine : Erreur(</title>
+    <link
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+rel="stylesheet"
+>
 </head>
+
 <body>
- <h1>Affichage des recettes</h1>
-    <ul>
-        <?php foreach (getRecipes($recipes) as $recipe) {
-         if ($recipe["is_enabled"]) {
-          echo '<h2>' . $recipe["title"] . '</h2>';
-          echo '<p>' . $recipe["recipe"] . '</p>';
-          echo displayAuthor($recipe["author"], $users);
-         }
-        } ?>
-    </ul>
+    <div class="container">
+        <h1>Liste des recettes de cuisine</h1>
+        <?php foreach(getRecipes($recipes) as $recipe) : ?>
+        <article>
+            <h3><?php echo($recipe['title']); ?></h3>
+            <div><?php echo($recipe['recipe']); ?></div>
+            <i><?php echo(displayAuthor($recipe['author'], $users)); ?></i>
+        </article>
+        <?php endforeach ?>
+    </div>
 </body>
 </html>
