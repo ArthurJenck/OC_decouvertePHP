f642b3381d467e549d20ac563010eea921ef417d
Arthur Jenck <arthurjenckdev@gmail.com>
2025-02-18 12:15:58 +0100

    feat: displayed list of users in for loop

--- index.php
+++ index.php
@@ -1,50 +1,25 @@
+<?php
+
+// Déclaration du tableau des recettes
+$recipes = [
+    ['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
+    ['Couscous','[...]','matthieu.nebra@openclassrooms.com',false,],
+    ['Blanquette','[...]','arthurjenckdev@gmail.com',false,],
+    ['Tajine','[...]','john.doe@gmail.com',true,],
+];
+
+?>
+
+<!DOCTYPE html>
 <html>
-
 <head>
- <meta charset="utf-8" />
- <title>Ceci est une page HTML de test</title>
+    <title>Affichage des recettes</title>
 </head>
-
 <body>
- <h1><?php 
-  $firstName = "Arthur";
-  $isEnabled = true
- ?>
- <?php echo "Bonjour {$firstName}" ?></h1>
- <p><?php 
-  if ($isEnabled):
-   echo "Vous pouvez accéder au site ✅";
-  
- ?></p>
- <h2>Page de test</h2>
- <p>
-  Cette page contient du code HTML avec des balises PHP.<br />
-  <?php echo "Ceci est du texte <br>" ?>
-  <?php echo("Ceci est \"aussi\" du <strong>texte</strong>" // Commentaire monoligne
-  ) ?>
-  <?php 
-   /*
-    Commentaire multiligne
-   */
-  ?>
-
- <br> 
- Voici quelques petits tests :
-
-
- </p>
-
- <ul>
-  <li style="color: blue;">Texte en bleu</li>
-  <li style="color: red;">Texte en rouge</li>
-  <li style="color: green;">Texte en vert</li>
- </ul>
-
- <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s') ?>.</p> 
-   <?php endif ;
-   if (!$isEnabled) {
-   echo "Accès refusé ❌";
-  }?>
+    <ul>
+        <?php for ($line = 0; $line < count($recipes); $line++): ?> 
+            <li><?php echo "{$recipes[$line][0]} ({$recipes[$line][2]})"; ?></li>
+        <?php endfor; ?>
+    </ul>
 </body>
-
 </html>
