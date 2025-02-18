28772b27e56ee9d33f53aa44c63c00306d9af75d
Arthur Jenck <arthurjenckdev@gmail.com>
2025-02-18 11:59:32 +0100

    feat: added conditions

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 </head>
 
 <body>
+ <h1><?php 
+  $firstName = "Arthur";
+  $isEnabled = true
+ ?>
+ <?php echo "Bonjour {$firstName}" ?></h1>
+ <p><?php 
+  if ($isEnabled):
+   echo "Vous pouvez accéder au site ✅";
+  
+ ?></p>
  <h2>Page de test</h2>
  <p>
   Cette page contient du code HTML avec des balises PHP.<br />
@@ -31,7 +41,10 @@
  </ul>
 
  <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s') ?>.</p> 
-
+   <?php endif ;
+   if (!$isEnabled) {
+   echo "Accès refusé ❌";
+  }?>
 </body>
 
 </html>
