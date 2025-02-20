a4cb70d3eb32b96a7f7b835a2d5b3a9b3db07471
Arthur Jenck <arthurjenckdev@gmail.com>
2025-02-20 15:06:39 +0100

    fix: moved folder

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
 <!-- header.php -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/tests">Site de recettes</a>
+        <a class="navbar-brand" href="/tests/Cours/">Site de recettes</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/tests/">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/tests/Cours/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/tests/contact/">Contact</a>
+                    <a class="nav-link" href="/tests/Cours/contact/">Contact</a>
                 </li>
             </ul>
         </div>
