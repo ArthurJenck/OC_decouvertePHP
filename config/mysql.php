bc37e6a9d5d08861bcf2b528fbd79e67b2f43bea
Arthur Jenck <arthurjenckdev@gmail.com>
2025-02-20 15:46:11 +0100

    feat: made variables arr dynamics

--- config/mysql.php
+++ config/mysql.php
@@ -1,7 +1,7 @@
 <?php
 
-try {
- $mysqlClient = new PDO('mysql:host=localhost;dbname=partage_de_recettes;charset=utf8', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-} catch (Exception $e) {
- die('Erreur : ' . $e->getMessage());
-}
+const MYSQL_HOST = 'localhost';
+const MYSQL_PORT = 3306;
+const MYSQL_NAME = 'partage_de_recettes';
+const MYSQL_USER = 'root';
+const MYSQL_PASSWORD = '';
